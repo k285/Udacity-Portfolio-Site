1982cc229fdf4e1822065059d20c716b4652bf55
Fangxiang Wang <direction.wfx@gmail.com>
2016-10-09 22:41:31 -0700

    sass->css

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) { 
+	grunt.loadNpmTasks('grunt-sass');
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				src: 'src/sass/style.scss',
+				dest: 'dist/css/style.css'
+			}
+		}
+	});
+	grunt.registerTask('default', [
+		'sass'
+	]);
+};
