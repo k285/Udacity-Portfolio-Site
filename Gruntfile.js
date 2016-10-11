a6340505a87fd53b4c94bc16b12559bf32c95a15
Fangxiang Wang <direction.wfx@gmail.com>
2016-10-11 00:17:44 -0700

    rwd image

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,27 @@ module.exports = function (grunt) {
 				src: config.scssDir + 'style.scss',
 				dest: config.cssDir + 'style.css'
 			}
-		}
+		},
+		responsive_images: {
+	      dev: {
+	        options: {
+	          engine: 'im',
+	          sizes: [{
+	            width: 800,
+	            suffix: '_small',
+	            quality: 50
+	          }]
+	        },
+	        files: [{
+	          expand: true,
+	          src: ['*.{jpg,}'],
+	          cwd: 'images_src/',
+	          dest: 'images/'
+	        }]
+	      }
+	    }
 	});
 	grunt.registerTask('default', [
-		'sass'
+		'sass', 'responsive_images'
 	]);
 };
