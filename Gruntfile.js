60dc0052a895b964bbbb9b80274733c34d23040b
direction123 <direction.wfx@gmail.com>
2016-11-11 03:34:57 -0800

    update portfolio

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,26 @@ module.exports = function (grunt) {
 	        options: {
 	          engine: 'im',
 	          sizes: [{
+	            width: 650,
+	            suffix: '_small',
+	            quality: 50
+	          }]
+	        },
+	        files: [{
+	          expand: true,
+	          src: ['portfolio*.png'],
+	          cwd: 'images_src/',
+	          dest: 'images/'
+	        }]
+	      },
+	      banner_image: {
+	        options: {
+	          engine: 'im',
+	          sizes: [{
+	            width: 1600,
+	            suffix: '_large',
+	            quality: 50
+	          },{
 	            width: 800,
 	            suffix: '_small',
 	            quality: 50
@@ -24,7 +44,27 @@ module.exports = function (grunt) {
 	        },
 	        files: [{
 	          expand: true,
-	          src: ['*.{jpg,}'],
+	          src: ['banner.jpg'],
+	          cwd: 'images_src/',
+	          dest: 'images/'
+	        }]
+	      },
+	      logo_image: {
+	        options: {
+	          engine: 'im',
+	          sizes: [{
+	            width: 140,
+	            suffix: '_large',
+	            quality: 50
+	          },{
+	            width: 100,
+	            suffix: '_small',
+	            quality: 50
+	          }]
+	        },
+	        files: [{
+	          expand: true,
+	          src: ['logo.png'],
 	          cwd: 'images_src/',
 	          dest: 'images/'
 	        }]
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Udacity-Portfolio-Site
+# Udacity Front-end Nanodegree - Project 
 
-## https://direction123.github.io/Udacity-Portfolio-Site/
+https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1
+
+## Portfolio Site
+
+To view this portfolio site, go to: https://direction123.github.io/Udacity-Online-Resume/ . It contains all the projects compeleted for this front-end developer nanodegree.
